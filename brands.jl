a034e1ce799ac966e4cb6abc9841fee18aa06fac
lingchen56 <33885457+lingchen56@users.noreply.github.com>
2018-11-28 22:04:31 -0800

    Add files via upload

---
+++ brands.jl
@@ -0,0 +1,214 @@
+{"name": "Anya Hindmarch"}
+{"name": "Ala\u00efa"}
+{"name": "Hunting Season"}
+{"name": "C\u00f4te&Ciel"}
+{"name": "Miu Miu"}
+{"name": "Kayu"}
+{"name": "Balenciaga"}
+{"name": "Marc Jacobs"}
+{"name": "Ienki Ienki"}
+{"name": "Marques'almeida"}
+{"name": "Versus"}
+{"name": "Ganni"}
+{"name": "Senreve"}
+{"name": "Acne Studios"}
+{"name": "Nico Giani"}
+{"name": "Corto Moltedo"}
+{"name": "Moncler Genius"}
+{"name": "Helmut Lang"}
+{"name": "Crash Baggage"}
+{"name": "Nathalie Trad"}
+{"name": "Valentino"}
+{"name": "rag & bone"}
+{"name": "Tory Burch"}
+{"name": "Calvin Klein 205W39nyc"}
+{"name": "Tod's"}
+{"name": "Adidas"}
+{"name": "Rebecca Minkoff"}
+{"name": "Vetements"}
+{"name": "Simon Miller"}
+{"name": "Marzook"}
+{"name": "Guidi"}
+{"name": "Dragon Diffusion"}
+{"name": "Maison Margiela"}
+{"name": "Smythson"}
+{"name": "Moschino Cheap & Chic"}
+{"name": "Ashish"}
+{"name": "See By Chlo\u00e9"}
+{"name": "GANNI"}
+{"name": "Sophie Anderson"}
+{"name": "Little Liffner"}
+{"name": "Sacai"}
+{"name": "CALPAK"}
+{"name": "Ann Demeulemeester"}
+{"name": "Kenzo"}
+{"name": "Rag & Bone"}
+{"name": "Altuzarra"}
+{"name": "Versace"}
+{"name": "The Row"}
+{"name": "Vanessa Bruno"}
+{"name": "Jacquemus"}
+{"name": "Etro"}
+{"name": "Yuzefi"}
+{"name": "Mulberry"}
+{"name": "Wandler"}
+{"name": "Sies Marjan"}
+{"name": "Dolce & Gabbana"}
+{"name": "Ea7 Emporio Armani"}
+{"name": "Thom Browne"}
+{"name": "Versace Jeans"}
+{"name": "Sensi Studio"}
+{"name": "Y-3"}
+{"name": "CALVIN KLEIN 205W39NYC"}
+{"name": "Emilio Pucci"}
+{"name": "JW Anderson"}
+{"name": "Coach"}
+{"name": "Sophia Webster"}
+{"name": "Oscar de la Renta"}
+{"name": "REJINA PYO"}
+{"name": "Bao Bao Issey Miyake"}
+{"name": "Heidi Klein"}
+{"name": "Cesta Collective"}
+{"name": "Ulla Johnson"}
+{"name": "Globe-Trotter"}
+{"name": "Ximena Kavalekas"}
+{"name": "Tom Ford"}
+{"name": "Lanvin"}
+{"name": "STAUD"}
+{"name": "Givenchy"}
+{"name": "Fila"}
+{"name": "Sara Battaglia"}
+{"name": "Stella McCartney"}
+{"name": "Porter-Yoshida & Co"}
+{"name": "Isabel Marant"}
+{"name": "Carolina Santo Domingo"}
+{"name": "Heron Preston"}
+{"name": "Max Mara"}
+{"name": "Adidas by Stella McCartney"}
+{"name": "Rick Owens"}
+{"name": "Twin-Set"}
+{"name": "Makavelic"}
+{"name": "Chiara Ferragni"}
+{"name": "BY FAR"}
+{"name": "Bottega Veneta"}
+{"name": "Staud"}
+{"name": "L'Autre Chose"}
+{"name": "A.P.C."}
+{"name": "Paul Smith"}
+{"name": "Danse Lente"}
+{"name": "Nannacay"}
+{"name": "Comme des Gar\u00e7ons Comme des Gar\u00e7ons"}
+{"name": "Alienina"}
+{"name": "Balmain"}
+{"name": "HEREU"}
+{"name": "Dries Van Noten"}
+{"name": "TOM FORD"}
+{"name": "Salvatore Ferragamo"}
+{"name": "Undercover"}
+{"name": "Comme des Gar\u00e7ons"}
+{"name": "Ookonn"}
+{"name": "Proenza Schouler"}
+{"name": "Jimmy Choo"}
+{"name": "Gucci"}
+{"name": "Mars\u00e8ll"}
+{"name": "Mm6 Maison Margiela"}
+{"name": "Lancaster"}
+{"name": "Henry Beguelin"}
+{"name": "Mostly Heard Rarely Seen 8-Bit"}
+{"name": "Chylak"}
+{"name": "Akris"}
+{"name": "THE VOLON"}
+{"name": "Loewe"}
+{"name": "Rejina Pyo"}
+{"name": "Saint Laurent"}
+{"name": "Burberry"}
+{"name": "Dorateymur"}
+{"name": "Bally"}
+{"name": "Brunello Cucinelli"}
+{"name": "Roger Vivier"}
+{"name": "Mansur Gavriel"}
+{"name": "Moncler"}
+{"name": "Mehry Mu"}
+{"name": "Dsquared2"}
+{"name": "McQ Alexander McQueen"}
+{"name": "Furla"}
+{"name": "Montunas"}
+{"name": "Paco Rabanne"}
+{"name": "3.1 Phillip Lim"}
+{"name": "Okhtein"}
+{"name": "No/An"}
+{"name": "Nancy Gonzalez"}
+{"name": "Polo Ralph Lauren"}
+{"name": "Tara Zadeh"}
+{"name": "Alexander McQueen"}
+{"name": "Moschino"}
+{"name": "Valextra"}
+{"name": "Sonia Rykiel"}
+{"name": "A.P.C. Atelier de Production et de Cr\u00e9ation"}
+{"name": "Ports 1961"}
+{"name": "Pinko"}
+{"name": "Fendi"}
+{"name": "Mark Cross"}
+{"name": "Kate Spade"}
+{"name": "Alessandra Rich"}
+{"name": "Carrie Forbes"}
+{"name": "Christopher Kane"}
+{"name": "Elizabeth and James"}
+{"name": "Zanellato"}
+{"name": "Chlo\u00e9"}
+{"name": "Marc Ellis"}
+{"name": "Off-White"}
+{"name": "NET-A-PORTER FANTASY GIFTS"}
+{"name": "Alexander Wang"}
+{"name": "Eastpak"}
+{"name": "Jil Sander"}
+{"name": "Nana-Nana"}
+{"name": "Paravel"}
+{"name": "Manu Atelier"}
+{"name": "Elisabetta Franchi"}
+{"name": "Natasha Zinko"}
+{"name": "Victoria Beckham"}
+{"name": "MZ Wallace"}
+{"name": "Cult Gaia"}
+{"name": "Karl Lagerfeld"}
+{"name": "Red Valentino"}
+{"name": "Theory"}
+{"name": "Manokhi"}
+{"name": "Fiorucci"}
+{"name": "Gabriela Hearst"}
+{"name": "Catzorange"}
+{"name": "Simone Rocha"}
+{"name": "Marni"}
+{"name": "Maison Kitsun\u00e9"}
+{"name": "Pintrill"}
+{"name": "Building Block"}
+{"name": "Roksanda"}
+{"name": "Muu\u00f1"}
+{"name": "Christian Louboutin"}
+{"name": "Eddie Harrop"}
+{"name": "ZOOBEETLE Paris"}
+{"name": "1017 Alyx 9SM"}
+{"name": "MANU Atelier"}
+{"name": "MCM"}
+{"name": "Loeffler Randall"}
+{"name": "Nanushka"}
+{"name": "Boyy"}
+{"name": "Prada"}
+{"name": "Golden Goose Deluxe Brand"}
+{"name": "Hillier Bartley"}
+{"name": "J\u00e9r\u00f4me Dreyfuss"}
+{"name": "Tumi"}
+{"name": "Attico"}
+{"name": "Shrimps"}
+{"name": "Gu_de"}
+{"name": "Calvin Klein 205W39NYC"}
+{"name": "Marine Serre"}
+{"name": "SIMON MILLER"}
+{"name": "Rochas"}
+{"name": "BOYY"}
+{"name": "Trademark"}
+{"name": "Michael Michael Kors"}
+{"name": "Wicker Wings"}
+{"name": "Olympia Le-Tan"}
+{"name": "Sanayi 313"}
+{"name": "Maje"}
